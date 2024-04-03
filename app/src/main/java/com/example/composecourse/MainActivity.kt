d833a658a269c66efd8526b47681b31efc24c0cf
HritikaNaik <hritikan232@gmail.com>
2024-04-03 12:42:24 +0530

    added to git

---
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.example.composecourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composecourse.ui.theme.ComposeCourseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCourseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CourseArticles()
+//                    TaskCompletedScreen()
+//                    ComposeQuandrantApp()
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CourseArticles(){
+    ArticleCard(
+        title = stringResource(R.string.title),
+        shortDescription = stringResource(R.string.short_decription),
+        longDescription = stringResource(R.string.long_description),
+        imageBanner = painterResource(R.drawable.banner),
+    )
+}
+
+@Composable
+fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imageBanner: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        Image(painter = imageBanner, contentDescription = "ImageBanner")
+        Text(text = title, modifier = Modifier.padding(16.dp), fontSize = 24.sp)
+        Text(text = shortDescription, modifier = Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+        Text(text = longDescription, modifier = Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+    }
+}
+
+@Composable
+fun TaskCompletedScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(painter = painterResource(R.drawable.ic_task_completed), contentDescription = "Task_completed")
+        Text(
+            text = stringResource(R.string.task_title),
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = stringResource(R.string.nice_work), fontSize = 16.sp)
+    }
+}
+
+@Composable
+fun ComposeQuandrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.title),
+                description = stringResource(R.string.short_decription),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.title),
+                description = stringResource(R.string.short_decription),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.title),
+                description = stringResource(R.string.short_decription),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.title),
+                description = stringResource(R.string.short_decription),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = title, modifier = Modifier.padding(bottom = 8.dp), fontWeight = FontWeight.Bold)
+        Text(text = description, textAlign = TextAlign.Justify)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeCoursePreview(){
+    ComposeCourseTheme{
+        CourseArticles()
+//        TaskCompletedScreen()
+//        ComposeQuandrantApp()
+        }
+}
